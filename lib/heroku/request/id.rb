8f04da6059555f91412ed436f0827189c829acbc
Jeremy Green <jeremy@octolabs.com>
2013-03-20 19:39:34 -0500

    initial commit

---
+++ lib/heroku/request/id.rb
@@ -0,0 +1,9 @@
+require "heroku/request/id/version"
+
+module Heroku
+  module Request
+    module Id
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/heroku/request/id/version.rb
@@ -0,0 +1,7 @@
+module Heroku
+  module Request
+    module Id
+      VERSION = "0.0.1"
+    end
+  end
+end
